43f520284ca0ab0687792a5a36ecbafb038dea42
Christian <habinezac15@gmail.com>
2024-01-22 11:25:18 +0200

    ft: event handler added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,7 +7,17 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 3;
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step === 1) return false;
+    setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    if (step === 3) return false;
+    setStep(step + 1);
+  };
 
   return (
     <div className="steps">
@@ -20,10 +32,16 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
